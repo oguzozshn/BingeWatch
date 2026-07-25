f8f16e38bf7307595365430ea0805254d4747ad9
Oguz Ozsahin <oguzozshn@gmail.com>
2026-07-25 15:10:03 +0300

    Add TMDb fallback for show and episode details

--- BingeWatch.Web/Models/ShowViewModelcs.cs
+++ BingeWatch.Web/Models/ShowViewModelcs.cs
@@ -1,4 +1,6 @@
-﻿namespace BingeWatch.Web.Models
+using System.Text.Json.Serialization;
+
+namespace BingeWatch.Web.Models
 {
     public class OmdbEpisodeRatingModel
     {
@@ -15,7 +17,6 @@
         public List<OmdbEpisodeRatingModel> Episodes { get; set; }
     }
 
-
     public class OmdbShowModel
     {
         public string Title { get; set; }
@@ -23,5 +24,53 @@
         public string Poster { get; set; }
         public string imdbRating { get; set; }
         public string imdbVotes { get; set; }
+        public string totalSeasons { get; set; }
+    }
+
+    public class TmdbShowDetailsModel
+    {
+        public string Name { get; set; }
+
+        [JsonPropertyName("first_air_date")]
+        public string FirstAirDate { get; set; }
+
+        [JsonPropertyName("poster_path")]
+        public string PosterPath { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonPropertyName("vote_count")]
+        public int VoteCount { get; set; }
+
+        public List<TmdbSeasonSummaryModel> Seasons { get; set; }
+    }
+
+    public class TmdbSeasonSummaryModel
+    {
+        [JsonPropertyName("season_number")]
+        public int SeasonNumber { get; set; }
+
+        [JsonPropertyName("episode_count")]
+        public int EpisodeCount { get; set; }
+    }
+
+    public class TmdbSeasonResponse
+    {
+        public List<TmdbEpisodeModel> Episodes { get; set; }
+    }
+
+    public class TmdbEpisodeModel
+    {
+        public string Name { get; set; }
+
+        [JsonPropertyName("season_number")]
+        public int SeasonNumber { get; set; }
+
+        [JsonPropertyName("episode_number")]
+        public int EpisodeNumber { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public double VoteAverage { get; set; }
     }
 }
